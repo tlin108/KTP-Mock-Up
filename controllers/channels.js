126a0a9677619afb2ec5f121a625a88d11a713bf
tlin108 <tlin108@hotmail.com>
2016-10-18 16:49:02 -0400

    get display by group in view working

--- controllers/channels.js
+++ controllers/channels.js
@@ -6,34 +6,40 @@ const _ = require('underscore');
 /* GET channel listing. */
 router.get('/', function(req, res, next) {
 
-  var timeArray = [];
   channels.forEach(function(channel) {
-
+    /*
     var dateTime = new Date(channel.time);
     var dateTimeFormat = {
       weekday: "short", year: "numeric", month: "short",
       day: "numeric", hour: "2-digit", minute: "2-digit"
     };
     var schedule = dateTime.toLocaleTimeString("en-us", dateTimeFormat).split(',');
+    */
 
     // remote date from channel time
-    var noDate = channel.time.split(' ');
-    channel.date = noDate[0];
-    channel.time = noDate[1];
+    var dateTime = channel.time.split(' ');
+    channel.date = dateTime[0];
+    channel.startTime = dateTime[1].toString();
+    var hourMin = dateTime[1].split(':');
+    channel.endTime = (parseInt(hourMin[0]) + 1).toString() + ":" + hourMin.splice(1, hourMin.length).join(':');
+
+    console.log(channel.date);
+    console.log(channel.startTime);
+    console.log(channel.endTime);
+
 
     //channel.date = schedule.splice(0, schedule.length-1).join(', ');
 
     // format channel start time to end time as period
+    /*
     var startTime = schedule[0].toString();
     var hourMin = schedule[0].split(':');
     var endTime = (parseInt(hourMin[0]) + 1).toString() + ":" + hourMin.splice(1, hourMin.length-1).join(', ') + " EDT";
     channel.period = startTime + " - " + endTime;
+    */
 
-    //console.log(channel.date);
-    //console.log(channel.period);
-    timeArray.push(channel);
   });
-  var sortByTime = _.sortBy(timeArray, 'time');
+  var sortByTime = _.sortBy(channels, 'time');
   var groupByDate = _.groupBy(sortByTime, 'date');
 
   var sortByDate = {}
@@ -42,7 +48,7 @@ router.get('/', function(req, res, next) {
   });
   console.log(sortByDate);
 
-  res.render('channels', { channel: sortByTime} );
+  res.render('channels', { channels: sortByDate} );
 });
 
 module.exports = router;
